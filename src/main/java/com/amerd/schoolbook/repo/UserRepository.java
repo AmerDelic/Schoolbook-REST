cd5cf8879b5af482db0adba5459c9df880b148d1
AmerDelic <75299077+amerdelic@users.noreply.github.com>
2022-10-07 12:13:22 +0200

    changing commit email

--- src/main/java/com/amerd/schoolbook/repo/UserRepository.java
+++ src/main/java/com/amerd/schoolbook/repo/UserRepository.java
@@ -1,14 +1,11 @@
 package com.amerd.schoolbook.repo;
 
 import com.amerd.schoolbook.domain.user.User;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    @NotNull Optional<User> findById(@NotNull Long id);
 
     Optional<User> findUserByEmail(String email);
 
